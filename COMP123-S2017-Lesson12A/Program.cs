9b1415b7a3bab0666e0fb24ddd4a16c7015faff5
300794883 <300794883@PR-A315-14>
2017-08-01 15:29:23 -0400

    Crated Demo

--- COMP123-S2017-Lesson12A/Program.cs
+++ COMP123-S2017-Lesson12A/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Name:Zilang Liang
+ * Date: August 1, 2017
+ * Description: This is a Demo Application to showcase User interface
+ * version 0.1 - created the project
+ */
+
 namespace COMP123_S2017_Lesson12A
 {
     static class Program
@@ -16,7 +23,7 @@ namespace COMP123_S2017_Lesson12A
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Demo());
         }
     }
 }
